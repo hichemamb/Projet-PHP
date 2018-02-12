6b1eaa7e9205e3b071cb2abdb844d02e57986c74
ShAd0wInK <alexandredelaloy@live.com>
2018-02-13 00:30:41 +0100

    modified social tab

--- www/index.php
+++ www/index.php
@@ -1,132 +1,134 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8"><meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/main.css">
-	<link rel="icon" href="img/icon.png">
+	<head>
+		<meta charset="UTF-8"><meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="icon" href="img/icon.png">
 
-	<title>KHAFQ.issou</title>
-</head>
-<body>
-	<header class="header">
-		<h1 class="header__title">KHAFQ.issou</h1>
-		<p class="header__description">Votre plateforme multifonction</p>
-		<div class="header__box">
-			<p class="h__box__name">Bonjour, <span>inconnu</span></p>
-			<img src="img/profil.svg" alt="user profil picture" class="h__box__img">
-		</div>
-	</header>
-	<section class="DaBigBox">
-		<section class="friends is-active-v">
-			<h2 class="friends__title">Ami(e)s :</h2>
-			<ul class="friends__list">
-				<li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li>
-				<li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li>
-				<li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li>
-				<li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li>
-				<li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li>
-				<li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li>
-				<li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li><li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li><li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li><li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li><li class="f__item">
-					<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
-					<p class="f__item__name">Quentin AIME</p>
-				</li>
-			</ul>
-		</section>
-		<section class="mainList">
-			<nav class="mainList__list">
-				<a href="#social" class="mL__list__item mL__social">Social</a>
-				<a href="#chat" class="mL__list__item mL__chat">Chat</a>
-				<a href="#forum" class="mL__list__item mL__forum">Forum</a>
-				<a href="#intranet" class="mL__list__item mL__intranet">Intranet</a>
-			</nav>
-			<section class="tabs">
-				<section id="social" class="tab mL__social is-active">
-					<section class="social__overflow">
-						<div class="social__post">
-						<img src="img/profil.svg" alt="post profil img" class="s__post__img">
-						<div class="s__box">
-							<h3 class="s__box__title">Title</h3>
-							<p class="s__box__user">User</p>
-							<p class="social__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nam quae eaque qui laudantium facere, sapiente aliquid suscipit commodi esse, sint sunt harum alias asperiores excepturi nihil in iure debitis?
+		<title>KHAFQ.issou</title>
+	</head>
+	<body>
+		<header class="header">
+			<h1 class="header__title">KHAFQ.issou</h1>
+			<p class="header__description">Votre plateforme multifonction</p>
+			<div class="header__box">
+				<p class="h__box__name">Bonjour, <span>inconnu</span></p>
+				<img src="img/profil.svg" alt="user profil picture" class="h__box__img">
+			</div>
+		</header>
+		<section class="DaBigBox">
+			<section class="friends is-active-v">
+				<h2 class="friends__title">Ami(e)s :</h2>
+				<ul class="friends__list">
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+					<li class="f__item">
+						<img src="img/profil.svg" alt="profil pciture" class="f__item__img">
+						<p class="f__item__name">Quentin AIME</p>
+					</li>
+				</ul>
+			</section>
+			<section class="mainList">
+				<nav class="mainList__list">
+					<a href="#social" class="mL__list__item mL__social">Social</a>
+					<a href="#chat" class="mL__list__item mL__chat">Chat</a>
+					<a href="#forum" class="mL__list__item mL__forum">Forum</a>
+					<a href="#intranet" class="mL__list__item mL__intranet">Intranet</a>
+				</nav>
+				<section class="tabs">
+					<section id="social" class="tab mL__social is-active">
+						<section class="social__overflow">
+							<div class="social__post">
+								<img src="img/profil.svg" alt="post profil img" class="s__post__img">
+								<div class="s__box">
+									<h3 class="s__box__title">Title</h3>
+									<p class="s__box__user">User</p>
+									<p class="social__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nam quae eaque qui laudantium facere, sapiente aliquid suscipit commodi esse, sint sunt harum alias asperiores excepturi nihil in iure debitis?
 
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde totam, sit similique recusandae. Consequatur repudiandae ea, voluptatem asperiores impedit? Sit quibusdam, perspiciatis ipsa? Sed consectetur non maiores ad, doloremque dicta?</p>
-						</div>
-						
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde totam, sit similique recusandae. Consequatur repudiandae ea, voluptatem asperiores impedit? Sit quibusdam, perspiciatis ipsa? Sed consectetur non maiores ad, doloremque dicta?</p>
+								</div>
+							</div>
+							<div class="social__post">
+								<img src="img/profil.svg" alt="post profil img" class="s__post__img">
+								<div class="s__box">
+									<h3 class="s__box__title">Title</h3>
+									<p class="s__box__user">User</p>
+									<p class="social__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nam quae eaque qui laudantium facere, sapiente aliquid suscipit commodi esse, sint sunt harum alias asperiores excepturi nihil in iure debitis?
 
-					</div>
-					<div class="social__post">
-						<img src="img/profil.svg" alt="post profil img" class="s__post__img">
-						<div class="s__box">
-							<h3 class="s__box__title">Title</h3>
-							<p class="s__box__user">User</p>
-							<p class="social__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nam quae eaque qui laudantium facere, sapiente aliquid suscipit commodi esse, sint sunt harum alias asperiores excepturi nihil in iure debitis?
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde totam, sit similique recusandae. Consequatur repudiandae ea, voluptatem asperiores impedit? Sit quibusdam, perspiciatis ipsa? Sed consectetur non maiores ad, doloremque dicta?</p>
+								</div>
+							</div>
+							<div class="social__post">
+								<img src="img/profil.svg" alt="post profil img" class="s__post__img">
+								<div class="s__box">
+									<h3 class="s__box__title">Title</h3>
+									<p class="s__box__user">User</p>
+									<p class="social__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nam quae eaque qui laudantium facere, sapiente aliquid suscipit commodi esse, sint sunt harum alias asperiores excepturi nihil in iure debitis?
 
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde totam, sit similique recusandae. Consequatur repudiandae ea, voluptatem asperiores impedit? Sit quibusdam, perspiciatis ipsa? Sed consectetur non maiores ad, doloremque dicta?</p>
-						</div>
-						
-
-					</div>
-					<div class="social__post">
-						<img src="img/profil.svg" alt="post profil img" class="s__post__img">
-						<div class="s__box">
-							<h3 class="s__box__title">Title</h3>
-							<p class="s__box__user">User</p>
-							<p class="social__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nam quae eaque qui laudantium facere, sapiente aliquid suscipit commodi esse, sint sunt harum alias asperiores excepturi nihil in iure debitis?
-
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde totam, sit similique recusandae. Consequatur repudiandae ea, voluptatem asperiores impedit? Sit quibusdam, perspiciatis ipsa? Sed consectetur non maiores ad, doloremque dicta?</p>
-						</div>
-						
-
-					</div>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde totam, sit similique recusandae. Consequatur repudiandae ea, voluptatem asperiores impedit? Sit quibusdam, perspiciatis ipsa? Sed consectetur non maiores ad, doloremque dicta?</p>
+								</div>
+							</div>
+						</section>
+						<section class="social__create">
+							<p class="s__create__text">Vous voulez partager une idée ? <a href="createpost.php" class="s__create__link">POSTER</a></p>
+						</section>
 					</section>
-					
+					<section id="chat" class="tab mL__chat"></section>
+					<section id="forum" class="tab mL__forum"></section>
+					<section id="intranet" class="tab mL__intranet"></section>
 				</section>
-				<section id="chat" class="tab mL__chat"></section>
-				<section id="forum" class="tab mL__forum"></section>
-				<section id="intranet" class="tab mL__intranet"></section>
+			</section>
+			<section class="profil">
+				<h2 class="profil__title">Inconnu</h2>
+				<ul class="profil__list">
+					<li class="p__list__item">Mes information</li>
+					<li class="p__list__item">Mes favoris</li>
+					<li class="p__list__item">Se déconnecter</li>
+				</ul>
 			</section>
 		</section>
-		<section class="profil">
-			<h2 class="profil__title">Inconnu</h2>
-			<ul class="profil__list">
-				<li class="p__list__item">Mes information</li>
-				<li class="p__list__item">Mes favoris</li>
-				<li class="p__list__item">Se déconnecter</li>
-			</ul>
-		</section>
-	</section>
-	<footer class="footer"></footer>
-</body>
-<script src="js/script.js"></script>
+		<footer class="footer">
+			
+		</footer>
+	</body>
+	<script src="js/script.js"></script>
 </html>
