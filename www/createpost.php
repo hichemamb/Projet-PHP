12707849856dcb259938a14e1140b1fefbeadb01
ShAd0wInK <alexandredelaloy@live.com>
2018-02-12 23:25:14 +0100

    add createpost.php

---
+++ www/createpost.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"><meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="icon" href="img/icon.png">
+
+	<title>KHAFQ.issou</title>
+</head>
+<body>
+	<header class="header">
+		<h1 class="header__title">KHAFQ.issou</h1>
+		<p class="header__description">Votre plateforme multifonction</p>
+		<div class="header__box">
+			<p class="h__box__name">Bonjour, <span>inconnu</span></p>
+			<img src="img/profil.svg" alt="user profil picture" class="h__box__img">
+		</div>
+	</header>
+	<section class="post">
+		<form action="" class="post__form">
+			<h2 class="p__form__title">Titre :</h2>
+			<input type="text" class="p__form__input__title" placeholder="Ex: Mon super titre">
+			<h2 class="p__form__description" placeholder="Mon super titre">Description :</h2>
+			<input type="text" class="p__form__input__description" placeholder="Ex: Ma super description">
+			<h2 class="p__form__tags">Tags :</h2>
+			<input type="text" class="p__form__input__tags" placeholder="Ex: PHP, Hagrid">
+			<input type="submit" class="p__form__submit">
+		</form>
+	</section>
+	<footer class="footer"></footer>
+
+</body>
+<script src="js/script.js"></script>
+</html>
